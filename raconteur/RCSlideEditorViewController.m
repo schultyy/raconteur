24fb8de3a491f6c32ab173c74ecc9255dde79de4
Jan Schulte <hello@unexpected-co.de>
2014-06-25 15:43:27 +0200

    Multiline text

--- raconteur/RCSlideEditorViewController.m
+++ raconteur/RCSlideEditorViewController.m
@@ -14,4 +14,29 @@
     return self;
 }
 
+/*
+ From https://developer.apple.com/library/mac/qa/qa1454/_index.htmlg
+ */
+- (BOOL)control:(NSControl*)control textView:(NSTextView*)textView doCommandBySelector:(SEL)commandSelector
+{
+    BOOL result = NO;
+    
+    if (commandSelector == @selector(insertNewline:))
+    {
+        // new line action:
+        // always insert a line-break character and don’t cause the receiver to end editing
+        [textView insertNewlineIgnoringFieldEditor:self];
+        result = YES;
+    }
+    else if (commandSelector == @selector(insertTab:))
+    {
+        // tab action:
+        // always insert a tab character and don’t cause the receiver to end editing
+        [textView insertTabIgnoringFieldEditor:self];
+        result = YES;
+    }
+    
+    return result;
+}
+
 @end
