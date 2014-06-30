f0a8efab244e22a180830a8a7e7ddb82027248a1
Jan Schulte <hello@unexpected-co.de>
2014-07-01 00:52:53 +0200

    Maximum height for html tag

--- raconteur/RCSlide.m
+++ raconteur/RCSlide.m
@@ -76,17 +76,50 @@
 
 #pragma mark - HTML
 
+-(NSString *) verticalTextAlign {
+
+    NSString *valign = self.options.verticalTextAlignment;
+    NSMutableString *style = [NSMutableString string];
+    [style appendString:@"margin-top: 50%"];
+
+//    if ([valign isEqualToString:RCVerticalTextAlignmentCenterValue]) {
+//        [style appendString:@".container { padding: 5% 0; }"];
+//        [style appendString:@".slide { padding: 10% 0; }"];
+//    }
+
+//    if([valign isEqualToString:RCVerticalTextAlignmentCenterValue]) {
+//        [style appendString:@"vertical-align: middle;"];
+//    }
+
+//    if([valign isEqualToString:RCVerticalTextAlignmentTopValue]) {
+//        [style appendString:@"top: 0%;\n"];
+//        [style appendString:@"transform: translateY(0%);\n"];
+//    }
+//    else if([valign isEqualToString:RCVerticalTextAlignmentCenterValue]) {
+//        [style appendString:@"top: 50%;\n"];
+//        [style appendString:@"transform: translateY(50%);\n"];
+//    }
+//    else if([valign isEqualToString:RCVerticalTextAlignmentBottomValue]) {
+//        [style appendString:@"top: 100%;\n"];
+//        [style appendString:@"transform: translateY(0%);\n"];
+//    }
+
+    return style;
+}
+
 -(NSString *) styles {
     NSString *fontFamily = [NSString stringWithFormat:@"font-family: %@;", self.options.fontFamily];
     NSString *backgroundColor = [NSString stringWithFormat:@"background-color:%@;", self.options.backgroundColor.hexColor];
     NSString *foregroundColor = [NSString stringWithFormat:@"color:%@;", self.options.foregroundColor.hexColor];
-    NSString *textAlignment = [NSString stringWithFormat:@"text-align: %@", self.options.horizontalTextAlignment];
+    NSString *horizontalTextAlignment = [NSString stringWithFormat:@"text-align: %@;", self.options.horizontalTextAlignment];
 
-    return [NSString stringWithFormat:@"body {\n" \
+
+    return [NSString stringWithFormat:@".slide {\n" \
                                 "%@\n" \
                                 "%@\n" \
                                 "%@\n" \
-                                "%@\n}", fontFamily, backgroundColor, foregroundColor, textAlignment];
+                                "%@\n" \
+                                "%@\n}", fontFamily, backgroundColor, foregroundColor, horizontalTextAlignment, self.verticalTextAlign];
 }
 
 -(NSString *)preview {
