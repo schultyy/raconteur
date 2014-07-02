aa085f0bca63a5f35cfeb5939168495f646a34ed
Jan Schulte <hello@unexpected-co.de>
2014-07-02 17:54:13 +0200

    remove comments

--- raconteur/RCPresentationBuilder.m
+++ raconteur/RCPresentationBuilder.m
@@ -39,19 +39,6 @@
 }
 
 -(NSString *) renderSlides {
-
-    /*
-    Slides cannot render their styles into the main style region.
-
-    Option 1:
-    Create custom class like `slide-01` and paste all related styles there
-
-    Option 2:
-    Write all styles on the corresponding div element for a slide like:
-    <div style="<styles here>" class='slide'>
-    </div>
-
-     */
     NSString *slidesHtml = [Underscore.arrayMap(self.project.slides, ^(RCSlide *slide) {
         return [self renderSlide:slide];
     }) componentsJoinedByString:@"\n"];
